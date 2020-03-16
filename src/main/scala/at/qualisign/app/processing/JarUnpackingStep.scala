b84fb4346eeaca4e3d8e716367fce157985510fb
jaichberg <jaichberg@gmail.com>
2020-03-16 11:08:20 +0100

    Distinguish between timeouts and other failures during JAR unpacking

--- src/main/scala/at/qualisign/app/processing/JarUnpackingStep.scala
+++ src/main/scala/at/qualisign/app/processing/JarUnpackingStep.scala
@@ -1,8 +1,10 @@
 package at.qualisign.app.processing
 
-import at.qualisign.domain.ProcessingStatus.{FAILED, PENDING, SUCCEEDED}
+import at.qualisign.core.exec.TimeoutException
+import at.qualisign.domain.ProcessingStatus.{FAILED, PENDING, SUCCEEDED, TIMEOUT_EXCEEDED}
 import at.qualisign.domain.Project
 import at.qualisign.unpacking.MavenProjectUnpacker
+import at.qualisign.unpacking.exceptions.UnpackingException
 
 class JarUnpackingStep(
   unpacker: MavenProjectUnpacker,
@@ -12,6 +14,13 @@ class JarUnpackingStep(
       unpacker.unpackProject(project)
       Right(project.copy(jarUnpackingStatus = SUCCEEDED))
     } catch {
+      case exception: UnpackingException =>
+        exception.getNestedException match {
+          case _: TimeoutException =>
+            Left(project.copy(jarUnpackingStatus = TIMEOUT_EXCEEDED), exception)
+          case _ =>
+            Left(project.copy(jarUnpackingStatus = FAILED), exception)
+        }
       case exception: Exception =>
         Left(project.copy(jarUnpackingStatus = FAILED), exception)
     }
