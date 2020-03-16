73f5c4582dbddd6c9b5c2edb05269cdf04dbad22
jaichberg <jaichberg@gmail.com>
2020-03-16 11:09:12 +0100

    Distinguish between timeouts and other failures during language detection

--- src/main/scala/at/qualisign/app/processing/LanguageDetectionStep.scala
+++ src/main/scala/at/qualisign/app/processing/LanguageDetectionStep.scala
@@ -1,8 +1,10 @@
 package at.qualisign.app.processing
 
-import at.qualisign.domain.ProcessingStatus.{FAILED, PENDING, SUCCEEDED}
+import at.qualisign.core.exec.TimeoutException
+import at.qualisign.domain.ProcessingStatus.{FAILED, SUCCEEDED, TIMEOUT_EXCEEDED}
 import at.qualisign.domain.Project
 import at.qualisign.languages.MavenProjectLanguageDetector
+import at.qualisign.languages.exceptions.LanguageDetectionException
 
 class LanguageDetectionStep(
   detector: MavenProjectLanguageDetector,
@@ -12,6 +14,13 @@ class LanguageDetectionStep(
       detector.detectProjectLanguages(project)
       Right(project.copy(languageDetectionStatus = SUCCEEDED))
     } catch {
+      case exception: LanguageDetectionException =>
+        exception.getNestedException match {
+          case _: TimeoutException =>
+            Left(project.copy(languageDetectionStatus = TIMEOUT_EXCEEDED), exception)
+          case _ =>
+            Left(project.copy(languageDetectionStatus = FAILED), exception)
+        }
       case exception: Exception =>
         Left(project.copy(languageDetectionStatus = FAILED), exception)
     }
